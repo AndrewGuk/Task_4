e990ac556f9a43e2dd0431bc074b43f1bd936f15
Andrei Huk <belakolala1@gmail.com>
2022-02-23 15:00:52 +0300

    create_generator

--- Tetris/Program.cs
+++ Tetris/Program.cs
@@ -7,18 +7,21 @@ namespace Tetris
     {
         static void Main(string[] args)
         {
-            int[,] place = new int[18, 17];
+            string[,] place = new string[10, 20];
             Random random = new Random();
-            string shablon = " | | ";
-
+            string shablon = "    ";
+            string block = "[][]";
+            //создаем место в которое генерируется куб
+            int vectorX = random.Next(0, place.GetLength(1));
             int valueShablon = shablon.Length;
+
             for (int i = 0; i < place.GetLength(0); i++)
             {
                 Write("$$");
                 for (int j = 0; j < place.GetLength(1); j++)
                 {
-                    place[i, j] = random.Next(0, 10);
-                    Write($" |{place[i, j]}| ");
+                    place[i, j] = shablon;
+                    Write($"{place[i, j]}");
                 }
                 Write("$$");
                 WriteLine();
@@ -33,6 +36,62 @@ namespace Tetris
             }
             Write("$$");
             WriteLine();
+
+            WriteLine();
+            place[0, vectorX] = block;
+            for (int i = 0; i < place.GetLength(0); i++)
+            {
+                Write("$$");
+                for (int j = 0; j < place.GetLength(1); j++)
+                {
+                    Write($"{place[i, j]}");
+                }
+                Write("$$");
+                WriteLine();
+            }
+            Write("$$");
+            for (int i = 0; i < place.GetLength(1); i++)
+            {
+                for (int j = 0; j < valueShablon; j++)
+                {
+                    Write("$");
+                }
+            }
+            Write("$$");
+            WriteLine();
+
+
+
+
+
+
+
+            //ИСПРАВНЫЙ ТАБЛИЦ:
+            //for (int i = 0; i < place.GetLength(0); i++)
+            //{
+            //    Write("$$");
+            //    for (int j = 0; j < place.GetLength(1); j++)
+            //    {
+            //        //place[i, j] = random.Next(0, 10);
+            //        //Write($" |{place[i, j]}| ");
+            //        Write(block);
+            //    }
+            //    Write("$$");
+            //    WriteLine();
+            //}
+            //Write("$$");
+            //for (int i = 0; i < place.GetLength(1); i++)
+            //{
+            //    for (int j = 0; j < valueShablon; j++)
+            //    {
+            //        Write("$");
+            //    }
+            //}
+            //Write("$$");
+            //WriteLine();
+
+
+
 
         }
 
