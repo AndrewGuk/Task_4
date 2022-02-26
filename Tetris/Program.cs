ed209e07c43ad66dd96a5cf3eaf4a4f6612588fb
Andrei Huk <belakolala1@gmail.com>
2022-02-26 13:16:25 +0300

    Tetris_game

--- Tetris/Program.cs
+++ Tetris/Program.cs
@@ -11,126 +11,16 @@ namespace Tetris
     {
         static void Main(string[] args)
         {
-            string[,] place = new string[10, 15];
+            string[,] place = new string[10,5];
             Random random = new Random();
             string shablon = "    ";
             string block = "[][]";
-            //создаем место в которое генерируется куб
             int vectorX; 
             int valueShablon = shablon.Length;
-            //ЗАПОЛНЕНИЕ ПОЛЯ
-            for (int i = 0; i < place.GetLength(0); i++)
-            {
-                Write("$$");
-                for (int j = 0; j < place.GetLength(1); j++)
-                {
-                    place[i, j] = shablon;
-                    Write($"{place[i, j]}");
-                }
-                Write("$$");
-                WriteLine();
-            }
-            Write("$$");
-            for (int i = 0; i < place.GetLength(1); i++)
-            {
-                for (int j = 0; j < valueShablon; j++)
-                {
-                    Write("$");
-                }
-            }
-            Write("$$");
-            ReadLine();
-            Clear();
-            
-            //работающий код
-            //for (int i = 0; ; i++)
-            //{
-            //    vectorX = random.Next(0, place.GetLength(1));
-
-            //    for (int j = 0; j < place.GetLength(0); j++)
-            //    {
-
-            //        place[j, vectorX] = block;
-            //        PlayingField();
-            //        if (j + 1 == place.GetLength(0))
-            //        {
-            //            break;
-            //        }
-            //        else if (place[j + 1, vectorX] == block)
-            //        {
-            //            break;
-            //        }
-            //        else if (j >= place.GetLength(0))
-            //        {
-            //            j = 0;
-            //        }
-            //        place[j, vectorX] = shablon;
-            //    }
-            //}
-            //конец
-            //впихиваем управление
-            for (int i = 0; ; i++)
-            {
-                vectorX = random.Next(0, place.GetLength(1));
-
-                for (int j = 0; j < place.GetLength(0); j++)
-                {
-
-                    place[j, vectorX] = block;
-                    vectorX = Control(j, vectorX);
-
-                    PlayingField();
-                    if (j + 1 == place.GetLength(0))
-                    {
-                        break;
-                    }
-                    else if (place[j + 1, vectorX] == block)
-                    {
-                        break;
-                    }
-                    else if (j >= place.GetLength(0))
-                    {
-                        j = 0;
-                    }
-                    place[j, vectorX] = shablon;
-                    
-
-                }
-            }
-            
-            //int Control(int j, int vector)
-            //{
-            //    string usEnter = ReadLine();
-            //    switch (usEnter)
-            //    {
-            //        case "d":
-            //            if (vector == place.GetLength(1) - 1)
-            //                break;
-            //            if (place[j, vector + 1] != shablon)
-            //                break;
-
-            //            place[j, vector + 1] = block;
-            //            place[j, vector] = shablon;
-            //            vector += 1;
-            //            break;
-            //        case "a":
-            //            if (vector == 0)
-            //                break;
-            //            if (place[j, vector - 1] != shablon)
-            //                break;
-
-            //            place[j, vector - 1] = block;
-            //            place[j, vector] = shablon;
-            //            vector -= 1;
-            //            break;
-            //        default:
-            //            break;
-            //    }
-            //    return vector;
-            //}
+            Zastavka();
             int Control(int j, int vector)
             {
-                var sec = 0.1;
+                var sec = 0.01;
                 bool bo = true;
                 do
                 {
@@ -158,49 +48,23 @@ namespace Tetris
                             place[j, vector + 1] = block;
                             place[j, vector] = shablon;
                             vector += 1;
+                            
+                        }
+                        else if (proba.Key == ConsoleKey.Escape)
+                        {
+                            Main(args);
                         }
                     }
-                    Sleep(100);
-                    if (sec++ > 0.1)
-                    {
+                    Beep();
+                    Sleep(10);
+                    if (sec++ > 0.01)
                         break;
-                    }
-
-
-
-                    //var press = ReadKey(true);
-                    //if (press.KeyChar == 'a' || press.KeyChar == 'A')
-                    //{
-                    //    if (vector == 0)
-                    //        return vector;
-                    //    if (place[j, vector - 1] != shablon)
-                    //        return vector;
-
-                    //    place[j, vector - 1] = block;
-                    //    place[j, vector] = shablon;
-                    //    vector -= 1;
-
-                    //}
-                    //else if (press.KeyChar == 'd' || press.KeyChar == 'D')
-                    //{
-                    //    if (vector == place.GetLength(1) - 1)
-                    //        return vector;
-                    //    if (place[j, vector + 1] != shablon)
-                    //        return vector;
-
-                    //    place[j, vector + 1] = block;
-                    //    place[j, vector] = shablon;
-                    //    vector += 1;
-                    //}
                     bo = false;
                 } while (bo);
                 return vector;
             }
-            
-
             void PlayingField()
             {
-                
                 for (int i = 0; i < place.GetLength(0); i++)
                 {
                     Write("$$");
@@ -220,16 +84,152 @@ namespace Tetris
                     }
                 }
                 Write("$$");
-
-                Sleep(1000);
+                CheckAndClean();
+                Sleep(500);
+                Clear();
+                
+            }
+            void CheckAndClean()
+            {
+                bool vs = false;
+                for (int i = place.GetLength(0)-1, j = 0; j < place.GetLength(1); j++)
+                {
+                    if (place[i, j] == shablon)
+                    {
+                        return;
+                    }
+                    vs = true;
+                    
+                }
+                if (vs)
+                {
+                    for (int i = place.GetLength(0)-1, j = 0; j < place.GetLength(1); j++)
+                    {
+                        place[i, j] = shablon;
+                    }
+                    for (int i = 1; i < place.GetLength(0)-1; i++)
+                    {
+                        for (int j = 0; j < place.GetLength(1); j++)
+                        {
+                            if (place[i,j] == block )
+                            {
+                                place[i, j] = shablon;
+                                place[i+1, j] = block;
+                            }
+                        }
+                    }
+                }
+            }
+            void PaintPlayingField()
+            {
+                for (int i = 0; i < place.GetLength(0); i++)
+                {
+                    Write("$$");
+                    for (int j = 0; j < place.GetLength(1); j++)
+                    {
+                        place[i, j] = shablon;
+                        Write($"{place[i, j]}");
+                    }
+                    Write("$$");
+                    WriteLine();
+                }
+                Write("$$");
+                for (int i = 0; i < place.GetLength(1); i++)
+                {
+                    for (int j = 0; j < valueShablon; j++)
+                    {
+                        Write("$");
+                    }
+                }
+                Write("$$");
                 Clear();
             }
+            void StartGame()
+            {
+                for (int i = 0; ; i++)
+                {
+                    vectorX = random.Next(0, place.GetLength(1));
+
+                    for (int j = 0; j < place.GetLength(0); j++)
+                    {
+
+                        place[j, vectorX] = block;
+                        vectorX = Control(j, vectorX);
+
+                        PlayingField();
+                        if (j + 1 == place.GetLength(0))
+                        {
+                            break;
+                        }
+                        else if (place[j + 1, vectorX] == block)
+                        {
+                            break;
+                        }
+                        else if (j >= place.GetLength(0))
+                        {
+                            j = 0;
+                        }
+                        place[j, vectorX] = shablon;
 
 
+                    }
+                }
+            }
+            void Play()
+            {
+                PaintPlayingField();
+                StartGame();
+            }
+            void Zastavka()
+            {
+                string[,] zastavka = new string[,] {
 
-
-
-
+                    {" "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," ", },
+                    {" ","#","#","#"," ","#","#","#"," ","#","#","#"," ","#","#","#"," ","#"," ","#","#","#"," "," "," "," "," ", },                   
+                    {" "," ","#"," "," ","#"," "," "," "," ","#"," "," ","#"," ","#"," ","#"," ","#"," "," "," "," "," "," "," ", },
+                    {" "," ","#"," "," ","#","#"," "," "," ","#"," "," ","#","#","#"," ","#"," ","#","#","#"," "," "," "," "," ", },
+                    {" "," ","#"," "," ","#"," "," "," "," ","#"," "," ","#"," ","#"," ","#"," "," "," ","#"," "," "," "," "," ", },                   
+                    {" "," ","#"," "," ","#","#","#"," "," ","#"," "," ","#"," ","#"," ","#"," ","#","#","#"," "," "," "," "," ", },
+                    {" "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," ", }
+                };
+                ForegroundColor = ConsoleColor.Green;
+                for (int i = 0; i < zastavka.GetLength(0); i++)
+                {
+                    for (int j = 0; j < zastavka.GetLength(1); j++)
+                    {
+                        
+                        Write(zastavka[i, j]);
+                    }
+                    WriteLine();
+                    Sleep(700);
+                }
+                ResetColor();
+                ForegroundColor = ConsoleColor.Cyan;
+                WriteLine("If you want to start playing enter: <play>" +
+                    "or <exit> to exit " +
+                    "\n Use A and D to control, press <esc> to exit");
+                ResetColor();
+                string answer = ReadLine().ToLower();
+                if (answer == "play")
+                {
+                    Play();
+                }
+                else if (answer == "exit")
+                {
+                    Exit();
+                }
+                else
+                {
+                    WriteLine("sorry( , unknown command ");
+                    Main(args);
+                }
+            }
+            void Exit()
+            {
+                ForegroundColor = ConsoleColor.Green;
+                WriteLine("Goodbye!");
+                ResetColor();
+            }
         }
 
     }
